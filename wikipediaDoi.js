3a5285b74a3f0a0df94867287f1f6e91b3dacabc
Gert van Valkenhoef <gvanvalkenhoef@cochrane.org>
2018-11-30 11:41:55 +0100

    Extract DOIs from the page, rather than the search snippet

--- wikipediaDoi.js
+++ wikipediaDoi.js
@@ -1,14 +1,15 @@
 const https = require('https');
 const fs = require('fs');
 const csv = require('csv');
+const async = require('async');
 const streamToString = require('./streamToString');
 
 function search(acc, offset, callback) {
   const query = 'insource:doi=10.1002/14651858';
-  const url = `https://en.wikipedia.org/w/api.php?action=query&format=json&list=search&srsearch=${query}&srwhat=text&srprop=snippet&srlimit=500&sroffset=${offset}`;
+  const url = `https://en.wikipedia.org/w/api.php?action=query&format=json&list=search&srsearch=${query}&srwhat=text&srlimit=500&sroffset=${offset}`;
   https.get(url, (res) => {
     if (res.statusCode != 200) {
-      callback(`HTTP code ${res.statusCode}`);
+      return callback(`HTTP code ${res.statusCode}`);
     }
     streamToString(res, (err, body) => {
       if (err) {
@@ -26,30 +27,40 @@ function search(acc, offset, callback) {
   });
 }
 
-const re = /(10.1002\/14651858.(CD|MR)[0-9]{6}(.pub[0-9]+)?)/i;
-function extractDoi(snippet) {
-  const clean = snippet.replace(/<span class="searchmatch">/g, '').replace(/<\/span>/g, '');
-  const match = clean.match(re);
-  if (match) {
-    return match[1];
-  } else {
-    console.log(clean);
-    return null;
-  }
+const re = /(14651858.(CD|MR)[0-9]{6}(.pub[0-9]+)?)/i; // not including "10.1002/" prefix to avoid dealing with urlencoding variations
+function getPageDois(page, callback) {
+  console.log(page.title);
+  const url = `https://en.wikipedia.org/w/api.php?action=parse&format=json&prop=externallinks&pageid=${page.pageid}`;
+  https.get(url, (res) => {
+    if (res.statusCode != 200) {
+      return callback(`HTTP code ${res.statusCode}`);
+    }
+    streamToString(res, (err, body) => {
+      if (err) {
+        return callback(err);
+      }
+      
+      const result = JSON.parse(body);
+      const links = result.parse.externallinks;
+      const matches = links.map((url) => url.match(re)).filter((m) => !!m).map((m) => "10.1002/" + m[1]);
+      callback(null, matches.map((m) => { return {
+        pageid: page.pageid,
+        title: page.title,
+        doi: m
+      }; }));
+    });
+  });
 }
+
+//getPageDois({ pageid: '1537', title: 'Acupuncture' }, function(err) { console.error(err); });
 
 search([], 0, (err, result) => {
   if (err) {
     return console.log(err);
   }
-  const mapped = result.map((page) => {
-    return {
-      pageid: page.pageid,
-      title: page.title,
-      doi: extractDoi(page.snippet)
-    };
-  });
-  csv.stringify(mapped, { columns: [ 'pageid', 'title', 'doi' ], quoted: true, header: true }, (err, csv) => {
-    fs.writeFileSync('wikipedia-dois.csv', csv, { 'encoding': 'utf-8' });
+  async.concatSeries(result, getPageDois, (err, mapped) => {
+    csv.stringify(mapped, { columns: [ 'pageid', 'title', 'doi' ], quoted: true, header: true }, (err, csv) => {
+      fs.writeFileSync('wikipedia-dois.csv', csv, { 'encoding': 'utf-8' });
+    });
   });
 });
